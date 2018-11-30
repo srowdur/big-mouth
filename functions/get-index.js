0a4db46f4842d2b373ed129125063b5231f6f3ee
Srinivas Rowdur <lonsr28@LONSR28-M1.local>
2018-11-30 12:20:38 +0000

    First static page has been created and added

--- functions/get-index.js
+++ functions/get-index.js
@@ -1,14 +1,25 @@
 'use strict';
 
-module.exports.handler = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
+const co = require("co");
+const Promise = require("bluebird");
+const fs =  Promise.promisifyAll(require("fs"));
+var html;
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
+function* loadHtml() {
+  if (!html) {
+    html = yield fs.readFileAsync('static/index.html', 'utf-8');
+  }
+
+  return html;
+}
+module.exports.handler = co.wrap (function * (event, context, callback) {
+  let loadhtml = yield loadHtml();
+  const response =  {
+    statusCode: 200,
+    body: html,
+    headers: {
+      'Content-Type': 'text/html; charset=UTF-8'
+    }
+  };
+  callback(null,response);
+});
